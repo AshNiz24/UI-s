2f96397844b99b79b97f93e68db1fb1a6cabe624
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2021-07-17 11:54:06 +0530

    Add files via upload

---
+++ YouTube UI/lib/vidPlay.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class YTVidPlayScreen extends StatefulWidget {
+  static const String id = 'YTVidPlayScreen';
+
+  @override
+  _YTVidPlayScreenState createState() => _YTVidPlayScreenState();
+}
+
+class _YTVidPlayScreenState extends State<YTVidPlayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
