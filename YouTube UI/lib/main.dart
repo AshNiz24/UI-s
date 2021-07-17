5f471e7226f58a7398eadfa17d0aaff19be81eb2
Ashna Nizam <68196153+AshNiz24@users.noreply.github.com>
2021-07-17 11:51:55 +0530

    Update main.dart

--- YouTube UI/lib/main.dart
+++ YouTube UI/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/home.dart';
+import 'package:youtube_clone/shorts_screen.dart';
+import 'package:youtube_clone/vidPlay.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      routes: {
+        HomeScreen.id: (context) => HomeScreen(),
+        YTVidPlayScreen.id: (context) => YTVidPlayScreen(),
+        ShortsScreen.id: (context) => ShortsScreen(),
+      },
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      initialRoute: HomeScreen.id,
     ),
   );
 }
